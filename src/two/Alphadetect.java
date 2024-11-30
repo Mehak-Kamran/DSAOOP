a80aa7aa71e9263b999f4e3762b42594d455adb9
Mehak Kamran <mehakkamran52@gmail.com>
2024-11-30 23:09:05 +0500

    initial commit

---
+++ src/two/Alphadetect.java
@@ -0,0 +1,16 @@
+package two;
+import java.util.Scanner;
+
+public class Alphadetect {
+	public static void main(String args[]) {
+		
+		System.out.println("Enter a char");
+		Scanner input=new Scanner(System.in);
+		String alp=input.next();
+		
+		
+		
+		
+	}
+
+}
